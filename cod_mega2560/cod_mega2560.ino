839ef3ce17f6fd84e049692ed91b19f6c19c1fb2
Richard do Carmo Lopes <32149536+RichardCLopes@users.noreply.github.com>
2024-09-23 00:35:51 -0300

    Update all sensors

--- cod_mega2560/cod_mega2560.ino
+++ cod_mega2560/cod_mega2560.ino
@@ -2,6 +2,8 @@
 #include <DHT.h>
 #include <EEPROM.h>
 #include "GravityTDS.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 
 //---------------SENSOR DE UMIDADE E TEMPERATURA DO AR--------------------
@@ -33,6 +35,28 @@ byte indiceUV=0; // Variavel para armazenar a conversão para indice UV
 int ph_pin = A4; 
 //------------------------------------------------------------------------
 
+//---------------------------SENSOR TERMOMETRO----------------------------
+#define ONE_WIRE_BUS 7 // Define pino do sensor
+OneWire oneWire(ONE_WIRE_BUS); // Cria um objeto OneWire
+DallasTemperature sensors(&oneWire); // Informa a referencia da biblioteca dallas temperature para Biblioteca onewire
+//------------------------------------------------------------------------
+
+//---------------------------SENSOR DE VAZÃO----------------------------
+//definicao do pino do sensor e de interrupcao
+const int INTERRUPCAO_SENSOR = 0; //interrupt = 0 equivale ao pino digital 2
+const int PINO_SENSORVZ = 2;
+
+//definicao de variaveis
+unsigned long contador = 0;
+const float FATOR_CALIBRACAO = 4.5; //
+float fluxo = 0;
+unsigned long tempo_antes = 0;
+
+void contador_pulso() {
+  contador++;
+}
+//------------------------------------------------------------------------
+
 
 void setup() {
   Serial.begin(115200); //Inicializa serial
@@ -45,7 +69,8 @@ void setup() {
   gravityTds.setAdcRange(4096);  //1024 for 10bit ADC;4096 for 12bit ADC
   gravityTds.begin();  //initialization
   pinMode(pinSensorUV, INPUT); //Sensor UV
-
+  pinMode(PINO_SENSORVZ, INPUT_PULLUP); //Sensor vazão
+  sensors.begin(); //Inicia o termometro
   
   delay(15000);
 }
@@ -67,34 +92,59 @@ void loop() {
 
   //LEITURA DO PH
   int measure = analogRead(ph_pin);
-  Serial.print("Measure: ");
-  Serial.print(measure);
-
+  //Serial.print("Measure: ");
+  //Serial.print(measure);
+  measure = measure - measure*0.15;
   double voltage = (5 / 1024.0) * measure; 
-  Serial.print("\tVoltage: ");
-  Serial.print(voltage, 3);
+  //Serial.print("\tVoltage: ");
+  //Serial.print(voltage, 3);
+  float PH_fim = 3.40;
+  float PH_ini = 2.75;
+  float PH_step = (PH_fim - PH_ini)/3;
+  float sph = 7 + ((PH_ini-voltage)/PH_step);
 
-  float sph = 7 + ((2.5 - voltage) / 0.18);
-  Serial.print("\tPH: ");
-  Serial.print(sph, 3);
-  Serial.println("");
+  //float sph = 7 + ((2.5 - voltage) / 0.18);
+  //Serial.print("\tPH: ");
+  //Serial.print(sph, 3);
+  //Serial.println("");
+  
+
+
+  //LEITURA TEMPERATURA DO LIQUIDO
+  sensors.requestTemperatures(); // Envia comando para realizar a conversão de temperatura
+  float templiq = sensors.getTempCByIndex(0);
+
+  //LEITURA DE VAZÃO
+  if((millis() - tempo_antes) > 1000){  
+  detachInterrupt(INTERRUPCAO_SENSOR); //desabilita a interrupcao para realizar a conversao do valor de pulsos
+  fluxo = ((1000.0 / (millis() - tempo_antes)) * contador) / FATOR_CALIBRACAO; //conversao do valor de pulsos para L/min
+
+  contador = 0; //reinicializacao do contador de pulsos
+  tempo_antes = millis(); //atualizacao da variavel tempo_antes
+  attachInterrupt(INTERRUPCAO_SENSOR, contador_pulso, FALLING); //contagem de pulsos do sensor    
+  }
   
   
    //verifica se foram lidos corretamente,
-   if (isnan(umidade) || isnan(tempar) || isnan(tds) || isnan(indiceUV) || isnan(sph)) {
+   if (isnan(tempar) || isnan(umidade) || isnan(tds) || isnan(indiceUV) || isnan(sph) || isnan(templiq) || isnan(fluxo)) {
     Serial.println("Falha na leitura dos sensores!");
   } else {
     // Envia os dados para o ESP8266 via Serial
-    Serial.print("Temperatura Ar:"); // Indicador de temperatura
-    Serial.println(tempar);
-    Serial.print("Umidade:"); // Indicador de umidade
-    Serial.println(umidade);
-    Serial.print("TDS:"); // Indicador de umidade
-    Serial.println(tds);
-    Serial.print("UV:"); // Indicador de umidade
-    Serial.println(indiceUV);
-    Serial.print("pH:"); // Indicador de umidade
-    Serial.println(sph);
+    Serial.print(" Temp. Ar: "); // Indicador de temperatura
+    Serial.print(tempar);
+    Serial.print(" ºC\t\tUmidade: "); // Indicador de umidade
+    Serial.print(umidade);
+    Serial.print(" %\t\tTDS :"); // Indicador de umidade
+    Serial.print(tds);
+    Serial.print(" Mg\\L\t\tUV: "); // Indicador de umidade
+    Serial.print(indiceUV);
+    Serial.print(" W\\M2\t\tpH: "); // Indicador de umidade
+    Serial.print(sph);
+    Serial.print(" ºC\t\tTemp. Liq: "); // Indicador de umidade
+    Serial.print(templiq);
+    Serial.print(" ºC\t\tVazao: "); // Indicador de umidade
+    Serial.print(fluxo);
+    Serial.print(" L\\min\n");
   }
 
 
@@ -113,6 +163,10 @@ void loop() {
   Serial3.print(indiceUV);
   Serial3.print(',');
   Serial3.print(sph);
+  Serial3.print(',');
+  Serial3.print(templiq);
+  Serial3.print(',');
+  Serial3.print(fluxo);
   Serial3.print('\n');
 
   //Espera 15 segundos para fazer o loop
